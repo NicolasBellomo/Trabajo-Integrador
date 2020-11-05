378d90cbfd48e8994b06774b8903310d80a7fd4e
Juan-2001 <jpantin@udesa.edu.ar>
2020-11-05 00:23:05 -0300

    ukit

--- jv/index.js
+++ jv/index.js
@@ -1,6 +1,18 @@
 window.onload = function() {
     fetch (`https://api.themoviedb.org/3/movie/550?api_key=0231890e16950b2b1fe941aec8ffe1d6`)
-    .then
+    .then (function (response) {
+        return respnse.json()
+    })
+    .then(function(data) {
+        console.log(data);
+        
+    })
+    
+    .catch(function(error){
+        console.log('el error fue: ' + error)
+    })
+
+
 
 
 
