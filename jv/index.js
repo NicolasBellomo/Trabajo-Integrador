d8b676a54c1b7f94becbe5b08458d922777a6277
Nicolas Bellomo <nicolasbellomo@MacBook-Pro-de-Nicolas.local>
2020-11-06 18:40:16 -0300

    Nada importante

--- jv/index.js
+++ jv/index.js
@@ -1,15 +1,18 @@
 window.onload = function() {
-    fetch (`https://api.themoviedb.org/3/movie/550?api_key=0231890e16950b2b1fe941aec8ffe1d6`)
+    var apikey= `0231890e16950b2b1fe941aec8ffe1d6`;
+    
+
+    fetch (`https://api.themoviedb.org/3/movie/550?api_key=${apikey}`)
     .then (function (response) {
         return respnse.json()
     })
-    .then(function(data) {
-        console.log(data);
+    .then(function(respuesta) {
+        console.log(respuesta);
         
     })
     
     .catch(function(error){
-        console.log('el error fue: ' + error)
+      console.log('el error fue: ' + error)
     })
 
 
