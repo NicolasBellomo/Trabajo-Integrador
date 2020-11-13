97b1a1496aee7ed6543caa9c147a9610464fb24f
Valen Ulloa <valenulloa@MacBook-Pro-de-Valen.local>
2020-11-13 11:57:35 -0300

    Rediseno homepage (ffaltan detalles)

--- jv/index.js
+++ jv/index.js
@@ -1,18 +1,75 @@
-window.onload = function() {
-    var apikey= `0231890e16950b2b1fe941aec8ffe1d6`;
-    
+window.addEventListener("load", function(){
 
-    fetch (`https://api.themoviedb.org/3/movie/550?api_key=${apikey}`)
-    .then (function (response) {
-        return respnse.json()
+    var url = `https://image.tmdb.org/t/p/w500`
+
+
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=eb09954096929ff16616027732037e32&language=en-US&page=1`)
+    .then(function (response) {
+        return response.json()
     })
-    .then(function(respuesta) {
-        console.log(respuesta);
+
+    .then(function (pelicula) {
+        
+        console.log(pelicula);
+
+        var vistas = document.querySelector("#mas-vistas")
+        
+
+        for (let i = 0; i < pelicula.results.length; i++) {
+            const element = pelicula.results[i];
+            
+           
+
+           var peliAgregar = `<li>
+           <img src= "${url}${element.poster_path}" alt="nn"/>
+           <div class="uk-position-center uk-panel"><h1></h1></div>
+       </li>`;
+
+            vistas.innerHTML += peliAgregar
+        }
+ 
+    })
+
+    .catch(function(error) {
+        console.log(`El error fue: ${error}`);
+    })
+
+
+
+
+
+
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=eb09954096929ff16616027732037e32&language=en-US&page=1`)
+    .then(function (response) {
+        return response.json()
+    })
+
+    .then(function (pelicula) {
+        
+        console.log(pelicula);
+
+        var vistas = document.querySelector("#ultimas")
+        
+
+        for (let i = 0; i < pelicula.results.length; i++) {
+            const element = pelicula.results[i];
+            
+           
+
+           var peliAgregar = `<li>
+           <img src= "${url}${element.poster_path}" alt="nn"/>
+           <div class="uk-position-center uk-panel"><h1></h1></div>
+       </li>`;
+
+            vistas.innerHTML += peliAgregar
+        }
+        
+        
         
     })
-    
-    .catch(function(error){
-      console.log('el error fue: ' + error)
+
+    .catch(function(error) {
+        console.log(`El error fue: ${error}`);
     })
 
 
@@ -22,9 +79,7 @@ window.onload = function() {
 
 
 
+    
 
 
-
-
-
-}
+})
