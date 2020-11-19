7a570cb7405315f989f0ad1d689aaf6ef580c445
Nicolas Bellomo <nicolasbellomo@192.168.1.68>
2020-11-19 00:21:09 -0300

    Buscador

--- jv/resultadobusqueda.js
+++ jv/resultadobusqueda.js
@@ -1,59 +1,43 @@
-window.addEventListener("load", function(){
-    
-    const API_KEY = `0231890e16950b2b1fe941aec8ffe1d6`;
-   //const API_KEY = `eb09954096929ff16616027732037e32`;
-    const IMAGE_URL = `https://image.tmdb.org/t/p/w500`; 
-     const url = `https://api.themoviedb.org/3/search/movie?api_key=0231890e16950b2b1fe941aec8ffe1d6`;
-    //const url = `https://api.themoviedb.org/3/movie/popular?api_key=eb09954096929ff16616027732037e32&language=en-US&page=`;
-
-const buttonElement = document.querySelector(`#search`);
-const InputElement = document.querySelector(`#inputValue`);
-const movieSearchable = document.querySelector(`movies-searchable`);
+let queryString = window.location.search;
+console.log(queryString);
+let queryObject = new URLSearchParams(queryString)
+let search = queryObject.get ('search');
+console.log(search);
 
 
-function movieSection(movies) {
-    return movie.map((movie) => {
-        return `
-        <img src=${IMAGE_URL +  movie.poster_path} data-movie-id=${movie.id}/>
-        `;
-    })
-}
+let apiKey = "0231890e16950b2b1fe941aec8ffe1d6"
+let url = `https://api.themoviedb.org/3/search/movie?api_key=0231890e16950b2b1fe941aec8ffe1d6`
 
-function createMovieContainer(movies) {
-    const movieElement = document.createElement(`div`);
-    movieElement.setAttribute(`class`, `movie`);
+let resultados = document.querySelector('.resultados')
 
-    const movieTemplate = `
-    <section class="section">
-    ${movieSection(movies)} 
-    </section>
-    <div class="content">
-     <p id="content-close">x</p>
-     </div>
-    `;
-    
-    movieElement.innerHTML = movieTemplate;
-    return movieElement; 
-}
-
-buttonElement.onclick = function(event) {
-    event.preventDefault();
-    const value = InputElement.value;
-
-    const newUrl = url + `&query=` + value;
-    
-    fetch(newUrl)
-    .then((res) => res.json())
-    .then((data) => {
-        const movies = data.results;
-        
-        
-        console.log(`data: `, data);
-    })
-    .catch((error) => {
-       console.log(`error: `, error);
-    });
-    console.log(`value: `, value);
-   
-}
+fetch(url)
+.then(function (respuestas){
+    return respuestas.json()
 })
+
+.then(function(data){
+    console.log(data);
+    let info = data.results;
+
+    for (let i=0; i<info.length; i++){
+        if(info[i].media_type == "tv"){
+            /* resultados.innerHTML += `<li>Serie: ${info[i].original_name}</li>` */
+            resultados.innerHTML += `<li>
+                                        <a href="detallep.html?id=${info[i].id}">
+                                            <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="">
+                                         </a>
+                                    </li>`
+        } else if (info[i].media_type == "movie"){
+            /* resultados.innerHTML += `<li>Pelicula: ${info[i].title}</li>` */
+
+            resultados.innerHTML += `<li>
+                                        <a href="detallep.html?id=${info[i].id}">
+                                            <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="">
+                                        </a>
+                                    </li>`
+        }
+        
+    }
+    
+})
+.catch(error => console.log(error))
